7fc9ed29e85feffd9d5b3c0617189836e3122aed
Colton Smith <coltonls24@gmail.com>
2020-09-03 14:34:23 -0500

    Challenge #2

--- main.js
+++ main.js
@@ -9,6 +9,17 @@ var employees = [
     ];
 
 // Part 2 Answer Here
+let organizedEmployees = {};
+let groups = [... new Set(employees.map(x => x.group))];
+
+groups.forEach(group => {
+    organizedEmployees[group] = [...
+        employees.filter(employee => employee.group === group)
+        .map(employee => employee.nameOrder === "reverse" ? employee.last + ' ' + employee.first : employee.first + ' ' + employee.last)
+    ] 
+});
+
+return organizedEmployees;
 
 /*****************************************************************************************
 * Bonus
